78169a2eff10fc4f8108749317d3e08e3094702b
jason lim <50891910+Sxxov@users.noreply.github.com>
2024-05-04 11:17:17 +0800

    Initial implementation of abstractions for fetching data

---
+++ src/lib/wordpress/getPageBySlug.ts
@@ -0,0 +1,30 @@
+import type { WP_REST_API_Page } from 'wp-types';
+
+/** Returns the first page that matches {@linkcode slug} */
+export async function getPageBySlug<T extends {}>(slug: string) {
+	'use server';
+
+	if (process.env.NODE_ENV === 'development')
+		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+	try {
+		const resp = await fetch(
+			`https://localhost/wp-json/wp/v2/pages?slug=${slug}`,
+		);
+
+		if (!resp.ok)
+			throw new Error(
+				`Failed to fetch page with slug ${slug}: ${resp.statusText}`,
+			);
+
+		const data = (await resp.json())[0] as WP_REST_API_Page & {
+			acf: {};
+		} & T;
+
+		return data;
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.error(e);
+		return undefined;
+	}
+}
