9f4a7b121c9aad9f91bfdfde0821468ae800c4a7
jiaSheng <50891910+sxxov@users.noreply.github.com>
2024-05-14 15:51:35 +0800

    Actual postbuild that works

--- tools/build/postbuild.js
+++ tools/build/postbuild.js
@@ -1,18 +1,39 @@
-import * as fs from 'node:fs/promises';
+import * as fs from 'node:fs';
+import * as p from 'node:path';
 
-cpIfExists('public', 'dist/public');
-cpIfExists('.next/standalone', 'dist');
-cpIfExists('.next/static', 'dist/.next/static');
+mkDirIfNotExists('dist');
 
-async function cpIfExists(
+cpDirIfExists('.next/standalone', 'dist');
+cpDirIfExists('public', 'dist/public');
+cpDirIfExists('.next/static', 'dist/.next/static');
+
+function mkDirIfNotExists(/** @type {string} */ path) {
+	if (!fs.existsSync(path)) {
+		fs.mkdirSync(path);
+	}
+}
+
+function cpDirIfExists(
 	/** @type {string} */ source,
 	/** @type {string} */ destination,
 ) {
-	try {
-		await fs.access(source);
-	} catch {
-		return;
-	}
+	if (!fs.existsSync(source)) return;
 
-	fs.cp(source, destination, { recursive: true, force: true });
+	// eslint-disable-next-line no-console
+	console.log(`copy: ${source} → ${destination}`);
+
+	cpR(source, destination);
+}
+
+function cpR(/** @type {string} */ src, /** @type {string} */ dest) {
+	const exists = fs.existsSync(src);
+	const stats = exists && fs.statSync(src);
+	const isDirectory = exists && stats.isDirectory();
+	if (isDirectory) {
+		fs.mkdirSync(dest, { recursive: true, mode: stats.mode });
+		for (const childItemName of fs.readdirSync(src))
+			cpR(p.join(src, childItemName), p.join(dest, childItemName));
+	} else {
+		fs.copyFileSync(src, dest);
+	}
 }
